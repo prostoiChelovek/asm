d1ae59c7a409329222919447a66b7923bf5ac879
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-08-15 21:54:39 +0500

    implement iteration over arguments

--- args-sum.s
+++ args-sum.s
@@ -2,22 +2,51 @@ global _start
 
 section .text                   
 _start:                         
-    pop rax                     ; rax = argc; stack pointer on argv now
+    pop rbx                     ; rbx = argc; stack pointer on argv now
 
-    mov rbx, [rsp + 8]          ; rbx = argv[1] - address of the second string array
+_args_loop:                     ; loop over the arguments in reverse order
+    cmp rbx, 0
+    je _args_loop_end           ; if (rbx != 0) finish the loop 
+    dec rbx
+    mov rax, [rsp + rbx * 8]    ; rax = argv[rbx]
+
     mov rcx, 0                  ; length
 _loop:
-    mov bpl, byte [rbx + rcx]   ; bpl = argv[0][rcx]
+    mov bpl, byte [rax + rcx]   ; bpl = argv[rax][rcx]
     inc rcx                     ; length++
     cmp bpl, 0x00
     jne _loop                   ; if (bpl != \0)
 
-    mov [arglen], rcx ; TODO use stack
+    mov r8, rax
+    mov [arglen], rcx
+    mov rax, 1                  ; write(
+    mov rdi, 1                  ;       fd=STDOUT,
+    mov rsi, r8                 ;       buf,
+    mov rdx, rcx                ;       count
+    syscall                     ; )
+
+    mov rax, 1                  ; write(
+    mov rdi, 1                  ;       fd=STDOUT,
+    mov rsi, space              ;       buf,
+    mov rdx, 1                  ;       count
+    syscall                     ; )
+
+    add byte [arglen], 48       ; convert the digit to ascii
     mov rax, 1                  ; write(
     mov rdi, 1                  ;       fd=STDOUT,
     mov rsi, arglen             ;       buf,
     mov rdx, 1                  ;       count
     syscall                     ; )
+
+    mov rax, 1                  ; write(
+    mov rdi, 1                  ;       fd=STDOUT,
+    mov rsi, newline            ;       buf,
+    mov rdx, 1                  ;       count
+    syscall                     ; )
+
+    jmp _args_loop
+
+_args_loop_end:
 
     mov rax, 60     ; exit (
     mov rdi, 0      ;       EXIT_SUCCESS
@@ -25,3 +54,8 @@ _loop:
 
 section .bss
 arglen: resb 4
+
+section .data
+newline: db `\n`
+space: db ` `
+
